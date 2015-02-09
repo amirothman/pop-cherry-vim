e8cf547f5472ae756e418625a62bdb09bcb60443
Amir Othman <th3go2@gmail.com>
2015-02-09 16:54:42 +0100

    create script.sh

---
+++ script.sh
@@ -0,0 +1,21 @@
+# Things To Do At A Fresh Vim
+
+## install pathogen
+     
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+## install NERDTree
+    
+    cd ~/.vim/bundle
+    git clone https://github.com/scrooloose/nerdtree.git
+
+## add smyck theme
+
+    git clone https://github.com/hukl/Smyck-Color-Scheme.git
+    mv smyck.vim cd ~/.vim/colors/smyck.vim
+
+## Create .vimrc
+
+    git clone https://github.com/amirothman/pop-cherry-vim.git
+    mv .vimrc ~/.vimrc
